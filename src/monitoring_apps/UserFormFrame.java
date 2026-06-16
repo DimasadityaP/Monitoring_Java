f75b71f97add013d801c0da5d2edf7544f7a6e68
LIO102 <auliodiva10@gmail.com>
2026-06-16 20:01:47 +0700

    Update user form new

--- src/monitoring_apps/UserFormFrame.java
+++ src/monitoring_apps/UserFormFrame.java
@@ -22,7 +22,6 @@ public class UserFormFrame extends javax.swing.JFrame {
     });
 }
     protected void kosong(){
-    txtId.setText("");
     txtNama.setText("");
     txtJabatan.setText("");
     txtTelepon.setText("");
@@ -57,8 +56,6 @@ public class UserFormFrame extends javax.swing.JFrame {
         btnClear = new components.RoundedButton();
         btnUpdate = new components.RoundedButton();
         btnBack = new components.RoundedButton();
-        txtId = new components.RoundedTextField();
-        lblPassword1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("FORM USER");
@@ -116,8 +113,6 @@ public class UserFormFrame extends javax.swing.JFrame {
             }
         });
 
-        lblPassword1.setText("Id Pengguna");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -138,7 +133,7 @@ public class UserFormFrame extends javax.swing.JFrame {
                         .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(20, 20, 20)
                         .addComponent(btnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblJabatan)
                             .addComponent(txtJabatan, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -150,15 +145,13 @@ public class UserFormFrame extends javax.swing.JFrame {
                             .addComponent(txtNama, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(lblPassword1, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(txtId, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(lblDivisi)
                             .addComponent(cmbDivisi, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblPassword)
                             .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblEmail)
-                            .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(11, 11, 11)))
                 .addContainerGap(40, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -176,25 +169,15 @@ public class UserFormFrame extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(lblNamaPengguna)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtNama, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblPassword1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtNama, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
                                 .addComponent(lblJabatan)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtJabatan, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(lblNomorTelepon)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtTelepon, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(lblAlamat)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtAlamat, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(txtTelepon, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(lblDivisi)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -207,6 +190,10 @@ public class UserFormFrame extends javax.swing.JFrame {
                                 .addComponent(lblEmail)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(18, 18, 18)
+                        .addComponent(lblAlamat)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtAlamat, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -225,52 +212,64 @@ public class UserFormFrame extends javax.swing.JFrame {
 
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         // TODO add your handling code here:
-        String sql = "insert into user (email, password, role) values (?,?,?)";
-        String id = txtId.getText();
-    String nama = txtNama.getText();
-    String jabatan = txtJabatan.getText();
+        String nama = txtNama.getText().trim();
+    String jabatan = txtJabatan.getText().trim();
     String divisi = cmbDivisi.getSelectedItem().toString();
-//    String telepon = txtTelepon.getText();
-//    String alamat = txtAlamat.getText();
-//    String email = txtEmail.getText();
-//    String password = txtPassword.getText();
-        try {
-            Connection conn = new KoneksiDb().connect();
-            PreparedStatement stat = conn.prepareStatement(sql);
-            stat.setString(1, id);
-            stat.setString(2, nama);
-            stat.setString(3, jabatan);
-//            stat.setString(4, divisi);
-//            stat.setString(5, telepon);
-//            stat.setString(6, alamat);
-//            stat.setString(7, email);
-//            stat.setString(8, password);
-            
-            
-            stat.executeUpdate();
-            JOptionPane.showMessageDialog(null, "Data Berhasil Disimpan");
-            kosong();
-            txtId.requestFocus();
+    String telepon = txtTelepon.getText().trim();
+    String alamat = txtAlamat.getText().trim();
+    String email = txtEmail.getText().trim();
+    String password = txtPassword.getText().trim();
 
-        }catch (SQLException e){
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-    
+    // Validasi form
+    if (nama.isEmpty() ||
+        jabatan.isEmpty() ||
+        telepon.isEmpty() ||
+        alamat.isEmpty() ||
+        email.isEmpty() ||
+        password.isEmpty() ||
+        divisi.equals("Pilih")) {
 
-    if(id.isEmpty() || nama.isEmpty()){
-        JOptionPane.showMessageDialog(this,
-                "Data belum lengkap!");
-    }else{
-        JOptionPane.showMessageDialog(this,
-                "Data berhasil disimpan");
-                
-        // Tambahkan query INSERT database di sini
+        JOptionPane.showMessageDialog(
+                this,
+                "Semua data wajib diisi!",
+                "Peringatan",
+                JOptionPane.WARNING_MESSAGE);
+
+        return; // Hentikan proses simpan
+    }
+
+    String sql = "INSERT INTO user (nama, jabatan, divisi, no_telp, alamat, email, password) VALUES (?,?,?,?,?,?,?)";
+
+    try {
+        Connection conn = new KoneksiDb().connect();
+        PreparedStatement stat = conn.prepareStatement(sql);
+
+        stat.setString(1, nama);
+        stat.setString(2, jabatan);
+        stat.setString(3, divisi);
+        stat.setString(4, telepon);
+        stat.setString(5, alamat);
+        stat.setString(6, email);
+        stat.setString(7, password);
+
+        stat.executeUpdate();
+
+        JOptionPane.showMessageDialog(
+                this,
+                "Data berhasil disimpan!",
+                "Sukses",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        kosong();
+        txtNama.requestFocus();
+
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(this, e.getMessage());
     }
     }//GEN-LAST:event_btnSaveActionPerformed
 
     private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
         // TODO add your handling code here:
-    txtId.setText("");
     txtNama.setText("");
     txtJabatan.setText("");
     txtTelepon.setText("");
@@ -294,38 +293,36 @@ public class UserFormFrame extends javax.swing.JFrame {
     private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateActionPerformed
         // TODO add your handling code here:
         String sql = "update user (email, password, role) values (?,?,?)";
-        String id = txtId.getText();
     String nama = txtNama.getText();
     String jabatan = txtJabatan.getText();
     String divisi = cmbDivisi.getSelectedItem().toString();
-//    String telepon = txtTelepon.getText();
-//    String alamat = txtAlamat.getText();
-//    String email = txtEmail.getText();
-//    String password = txtPassword.getText();
+    String telepon = txtTelepon.getText();
+    String alamat = txtAlamat.getText();
+    String email = txtEmail.getText();
+    String password = txtPassword.getText();
         try {
             Connection conn = new KoneksiDb().connect();
             PreparedStatement stat = conn.prepareStatement(sql);
-            stat.setString(1, id);
             stat.setString(2, nama);
             stat.setString(3, jabatan);
-//            stat.setString(4, divisi);
-//            stat.setString(5, telepon);
-//            stat.setString(6, alamat);
-//            stat.setString(7, email);
-//            stat.setString(8, password);
+            stat.setString(4, divisi);
+            stat.setString(5, telepon);
+            stat.setString(6, alamat);
+            stat.setString(7, email);
+            stat.setString(8, password);
             
             
             stat.executeUpdate();
             JOptionPane.showMessageDialog(null, "Data Berhasil Diubah");
             kosong();
-            txtId.requestFocus();
+            txtNama.requestFocus();
 
         }catch (SQLException e){
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     
 
-    if(id.isEmpty() || nama.isEmpty()){
+    if(nama.isEmpty()){
         JOptionPane.showMessageDialog(this,
                 "Data belum lengkap!");
     }else{
@@ -350,12 +347,10 @@ public class UserFormFrame extends javax.swing.JFrame {
     private javax.swing.JLabel lblNamaPengguna;
     private javax.swing.JLabel lblNomorTelepon;
     private javax.swing.JLabel lblPassword;
-    private javax.swing.JLabel lblPassword1;
     private components.PageTitle pageTitle1;
     private components.SidebarMenu sidebarMenu1;
     private components.RoundedTextField txtAlamat;
     private components.RoundedTextField txtEmail;
-    private components.RoundedTextField txtId;
     private components.RoundedTextField txtJabatan;
     private components.RoundedTextField txtNama;
     private components.RoundedTextField txtPassword;
